b3f4b9b03afaf38874269e57f499e9939b15b05e
linxiaohui <llinxiaohui@126.com>
2017-02-27 16:31:39 +0800

    IMPORTANT: POM updates/DataStream

--- flink/src/main/scala/api/dataset/DataSetProcess.scala
+++ flink/src/main/scala/api/dataset/DataSetProcess.scala
@@ -1,7 +1,22 @@
+/*
+ * For Flink, batch processing is a special case of stream processing.
+ * 
+ * */
+
 package api.dataset
+
+import org.apache.flink.api.scala._
+import org.apache.flink.api.scala.extensions._
+
 
 object DataSetProcess {
     def main(args:Array[String]) {
         println("DataSet Example")
+        
+        //
+        /* create a local/remote environment */
+        val env = ExecutionEnvironment.getExecutionEnvironment
+        
+
     }
 }
--- flink/src/main/scala/api/datastream/DataStreamProcess.scala
+++ flink/src/main/scala/api/datastream/DataStreamProcess.scala
@@ -1,7 +1,24 @@
 package api.datastream
 
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.extensions._
+import org.apache.flink.streaming.api.windowing.time.Time
+
 object DataStreamProcess {
-    def main(args:Array[String]) {
-        println("DataStream Example")
-    }
+  def main(args: Array[String]) {
+    println("DataStream Example")
+
+    //Obtain Execution environment
+    /* create a local/remote environment
+    val local = ExecutionEnvironment.createLocalEnvironment()
+    val remote = ExecutionEnvironment.createRemoteEnvironment("locahost",9300,"a.jar")
+    */
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val input = env.socketTextStream("localhost", 1234)
+    val out = input.map((x:String)=> x.toInt)
+
+    env.execute("Window Stream WordCount")
+
+  }
 }
