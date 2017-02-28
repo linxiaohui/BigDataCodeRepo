3eab4b8ed5cb45a38d147e278267d55761658d9a
linxiaohui <llinxiaohui@126.com>
2017-02-28 10:11:16 +0800

    flink. DataStream

--- flink/src/main/scala/api/datastream/DataStreamProcess.scala
+++ flink/src/main/scala/api/datastream/DataStreamProcess.scala
@@ -14,11 +14,22 @@ object DataStreamProcess {
     val remote = ExecutionEnvironment.createRemoteEnvironment("locahost",9300,"a.jar")
     */
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val env2 = StreamExecutionEnvironment.getExecutionEnvironment
 
     val input = env.socketTextStream("localhost", 1234)
-    val out = input.map((x:String)=> x.toInt)
-
-    env.execute("Window Stream WordCount")
-
+    val out = input.map { x=>
+      val in=x.split(" ")
+      (in(0), in(1).toInt)
+    }.keyBy(_._1).reduce { (x,y) =>
+      (x._1, x._2+y._2)
+    }
+    out.print()
+    val input2 = env2.socketTextStream("localhost",12345)
+    val out2 = input2.map(x=>(1,x.toInt)).keyBy(_._1).reduce{ (x,y) =>
+      (x._1,x._2+y._2)
+    }.map(_._2)
+    out2.print()
+    //env.execute("Stream WordCount") // Will Nerver Return ?
+    env2.execute("Stream WC")
   }
 }
